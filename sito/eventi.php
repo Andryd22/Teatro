b017b3c44dcd70b7bc8e8d84b99e27feffc3fe77
ANDRYD-PC1\andry <andry2d2@gmail.com>
2023-04-11 23:18:35 +0200

    CSS finally risolto buggettini

--- sito/eventi.php
+++ sito/eventi.php
@@ -8,29 +8,29 @@
     <link rel="icon" href="img/favicon.png">
 
     <title>Registrazione Online</title>
-    <link rel="stylesheet" href="stile.css" type="text/css">
+    <link rel="stylesheet" href="./styles/eventi.css" type="text/css">
+    
+    <!-- href="./stile.css"  -->
+    <!-- href="./styles/eventi.css"
+    
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-
+-->
 </head>
 
 
 <body>
     <nav class="navbar navbar-expand-md">
         <a class="navbar-brand" href="index.html"><img src="img/logo.png" /></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-            <i class="fas fa-bars"></i>
-        </button>
-
+       
         <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="index.html"><i class="fas fa-home"></i> Home</a>
-                </li>
-            </ul>
+            
+            <a class="nav-button" href="index.html"> Home</a>
+
             <a class="nav-button" href="register.php"> Registrati</a>
+
         </div>
     </nav>
    
