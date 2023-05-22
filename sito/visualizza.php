acc1fe6e042455bf560d0e8d602dda670c316b63
ANDRYD-PC2\Andrea <andry2d2@gmail.com>
2023-05-22 11:39:18 +0200

    inizio validazione 2 nuovi buggettini roba che viene caricarata anche se non è presente

--- sito/visualizza.php
+++ sito/visualizza.php
@@ -10,12 +10,7 @@
     <title>Visualizza</title>
     <link rel="stylesheet" href="./styles/visualizza.css" type="text/css">
 
-    <!-- ./styles/visualizza.css
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
--->
+    
 </head>
 
 <?php
